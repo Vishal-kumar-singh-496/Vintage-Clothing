c166ea19da1aaaa49fdc6f7f9cf5efc410af05ae
Vishal-Kumar-singh-496 <vishalramgarh9934@gmail.com>
2023-02-22 18:43:16 +0530

    Made some changes for routing

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,10 +1,15 @@
 import './directory-item.styles.scss';
+import {useNavigate} from 'react-router-dom';
 const DirectoryItem=({category})=>{
     
 
-    const {title,imageUrl}=category;
+    const {title,imageUrl,route}=category;
+
+const navigate=useNavigate();
+
+const navigateHandler=()=>navigate(route);
     return(
-    <div  className='directory-item-container'>
+    <div  className='directory-item-container' onClick={navigateHandler}>
       <div className='background-image' style={{backgroundImage:`url(${imageUrl})`
     }}/>
        <div className='body'>
